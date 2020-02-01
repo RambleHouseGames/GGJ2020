8d889f0aa2744b47eee53fe53eded7f18b74d748
situtyo <situtyo666@gmail.com>
2020-02-02 02:08:57 +0900

    change start button

---
+++ Assets/Scripts/Title/UIFlash.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIFlash : MonoBehaviour
+{
+    private enum State
+    {
+        None,
+        Flash,
+    }
+
+    [SerializeField]
+    Text _text = null;
+
+    Color _initColor;
+
+    StateMachine<State> _stateMachine = new StateMachine<State>();
+
+    float _flashScale = 1.0f;
+
+    void Awake()
+    {
+        _initColor = _text.color;
+        SetupState();
+    }
+
+    void Update()
+    {
+        _stateMachine.UpdateState();
+    }
+
+    public void Play(float flashScale = 1.0f)
+    {
+        _flashScale = flashScale;
+        _stateMachine.ChangeState(State.Flash);
+    }
+
+    public void Stop()
+    {
+        _stateMachine.ChangeState(State.None);
+    }
+
+    private void SetupState()
+    {
+        SetupStateNone();
+        SetupStateFlash();
+        _stateMachine.ChangeState(State.None);
+    }
+
+    private void SetupStateNone()
+    {
+        var state = State.None;
+        Action<State> enter = (prev) => { };
+        Action update = () => { };
+        Action<State> exit = (next) => { };
+        _stateMachine.AddState(state, enter, update, exit);
+    }
+
+    private void SetupStateFlash()
+    {
+        var state = State.Flash;
+        Action<State> enter = (prev) =>
+        {
+        };
+        Action update = () =>
+        {
+            var color = _initColor;
+            color.a = Mathf.Abs(Mathf.Sin(Time.time * _flashScale));
+            _text.color = color;
+        };
+        Action<State> exit = (next) =>
+        {
+            _text.color = _initColor;
+        };
+        _stateMachine.AddState(state, enter, update, exit);
+    }
+
+
+}
---
+++ Assets/Scripts/Utility/GameTimer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameTimer
+{
+    float _currentTime;
+    float _limitTime;
+
+    public bool IsTimeUp
+    {
+        get
+        {
+            return _currentTime >= _limitTime;
+        }
+    }
+
+    public float TimeRate
+    {
+        get
+        {
+            return Mathf.Min(1.0f, _currentTime / _limitTime);
+        }
+    }
+
+    public GameTimer(float time)
+    {
+        _limitTime = time;
+    }
+
+    public void ResetTimer()
+    {
+        _currentTime = 0.0f;
+    }
+
+    public bool UpdateTimer(float scale = 1.0f)
+    {
+        _currentTime += Time.deltaTime * scale;
+        return IsTimeUp;
+    }
+}
