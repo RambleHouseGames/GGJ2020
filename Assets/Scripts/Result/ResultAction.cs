47fd7418ef8f938840126333a0f524f73eb0ea4a
kibougouz <kenta15901@yahoo.co.jp>
2020-02-01 16:08:11 +0900

    add Result

--- Assets/Scripts/Result/ResultAction.cs
+++ Assets/Scripts/Result/ResultAction.cs
@@ -8,14 +8,18 @@ public class ResultAction : MonoBehaviour
 {
     //Enter an arbutrary name
     public InputField PlayerName;
-    public Text PlayerNameText;
+    public Text PlayerNameTextN;
 
     //Score Display
-    public Text[] TotalScoreRank = new Text [5];
+    public int TotalScore;
+    public Text HitScore;
+    public Text MissScore;
+    public Text RankScore;
+    /*public Text[] TotalScoreRank = new Text [5];
     public Text[] HitScore = new Text[5];
-    public Text[] MissScore = new Text[5];
+    public Text[] MissScore = new Text[5];*/
     float Elapsed = 0.0f;
-    public string Score = "00000";
+    public int Score = 0;
     public string Score1 = "00000";
     public string Score2 = "00000";
 
@@ -24,37 +28,40 @@ public class ResultAction : MonoBehaviour
     {
         //Enter an arbutrary name
         PlayerName = PlayerName.GetComponent<InputField>();
-        PlayerNameText = PlayerNameText.GetComponent<Text>();
+        PlayerNameTextN = PlayerNameTextN.GetComponent<Text>();
 
         //Score Display
-        for (int idx = 0; idx < 5; idx++) {
-            TotalScoreRank[idx].text = Score;
-            HitScore[idx].text = Score1;
-            MissScore[idx].text = Score2;
-            //if (PlayerPrefs.GetFloat("R" + idx) >= float.MaxValue) {
-            //    txtRank[idx].text = "_.__s";
-            //} else {
-            //    txtRank[idx].text = PlayerPrefs.GetFloat("R" + idx).ToString("f2") + "s";
-            //}
-        }
+        TotalScore = Score;
+        HitScore.text = Score1;
+        MissScore.text = Score2;
+        //for (int idx = 0; idx < 5; idx++) {
+        //TotalScoreRank[idx].text = Score;
+        //HitScore[idx].text = Score1;
+        //MissScore[idx].text = Score2;
+        //if (PlayerPrefs.GetFloat("R" + idx) >= float.MaxValue) {
+        //    txtRank[idx].text = "_.__s";
+        //} else {
+        //    txtRank[idx].text = PlayerPrefs.GetFloat("R" + idx).ToString("f2") + "s";
+        //}
+        //}
     }
 
     public void InputText()
     {
         //テキストにinputFieldの内容を反映
-        PlayerNameText.text = PlayerName.text;
-
+        PlayerNameTextN.text = PlayerName.text;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
         if (Input.GetButton("Fire1"))
         {
             Elapsed += Time.deltaTime; //画面を押し続けている間ずっと   
             if (Elapsed > 3.0f)
             {
+                PlayerPrefs.SetInt("Score", Score);
+                PlayerPrefs.GetString("Name", "no name");
                 SceneManager.LoadScene("Title");
             }
         }
