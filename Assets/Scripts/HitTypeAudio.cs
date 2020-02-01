76bec91e7199b7491953d13f1843566d92907463
Scott <swsteffes@gmail.com>
2020-02-01 21:38:59 +0900

    C,C,Changes

---
+++ Assets/Scripts/HitTypeAudio.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class HitTypeAudio : MonoBehaviour {
+    public AudioSource hitSourceLeft;
+    public AudioSource hitSourceRight;
+    public AudioClip greatHitClip;
+    public AudioClip okHitClip;
+    public AudioClip missHitClip;
+
+    public void PlayHitSound(HitType hitType, bool left) {
+        AudioSource hitSource = left ? hitSourceLeft : hitSourceRight;
+        AudioClip hitClip;
+        switch (hitType) {
+            case HitType.Great:
+                hitClip = greatHitClip;
+                break;
+            case HitType.OK:
+                hitClip = okHitClip;
+                break;
+            case HitType.Miss:
+                hitClip = missHitClip;
+                break;
+            default:
+                hitClip = missHitClip;
+                break;
+        }
+        hitSource.PlayOneShot(hitClip);
+    }
+}
